4d8930cb6bf536b14c9843308d1dba01d57461b9
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-10-20 13:38:53 -0700

    on close alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
 import { Button } from "./components/Button";
-import ListGroup from "./components/ListGroup";
 
 function App() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
+  const [alertVisible, setAlertVisible] = useState(false);
+
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
-      <Alert>
-        <strong>Hello World</strong>
-      </Alert>
-      <Button color="danger" onClick={() => console.log("clicked")}>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisible(false)}>Alert Alert Alert</Alert>
+      )}
+      <Button color="danger" onClick={() => setAlertVisible(true)}>
         My Button
       </Button>
     </div>
