69a25f21de36e85f46ea702a1602ac0bf00b13f8
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-10-20 12:34:34 -0700

    render list conditional

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,19 @@
 function ListGroup() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
+
   return (
-    <div>
-      <h1>List Group</h1>
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li key={item} className="list-group-item">
+            {item}
+          </li>
+        ))}
       </ul>
-    </div>
+    </>
   );
 }
 
