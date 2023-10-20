b16ab5bbfab0fce929edde6d26dea51392f9a699
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-10-20 12:53:58 -0700

    create cities and header props

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import ListGroup from "./components/ListGroup";
 
-
 function App() {
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
   return (
     <div>
-      <ListGroup />
+      <ListGroup items={items} heading="Cities" />
     </div>
-  )
+  );
 }
 
 export default App;
